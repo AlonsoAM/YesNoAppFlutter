c7c4074b9f32705f1c3d9ded8fcc798309442db2
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-06-15 13:38:42 -0500

    Mostrar los mensajes del provider

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../../domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  final List<Message> _messages = [
+  final List<Message> messagesList = [
     Message(text: 'Hola', fromWho: FromWho.me),
     Message(text: 'Ya volviste del trabajo?', fromWho: FromWho.me),
   ];
--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+import '../../../domain/entities/message.dart';
+import '../../providers/chat_provider.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -26,6 +30,8 @@ class ChatScreen extends StatelessWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -33,11 +39,12 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
                 child: ListView.builder(
-              itemCount: 100,
+              itemCount: chatProvider.messagesList.length,
               itemBuilder: (context, index) {
-                return (index % 2 == 0)
-                    ? const HerMessageBubble()
-                    : const MyMessageBubble();
+                final message = chatProvider.messagesList[index];
+                return message.fromWho == FromWho.me
+                    ? MyMessageBubble()
+                    : HerMessageBubble();
               },
             )),
             const MessageFieldBox()
