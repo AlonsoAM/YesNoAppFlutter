458d3db319524566c47952aee3df2c89a22c5f6c
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-06-15 13:14:57 -0500

    Crear el chat provider

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../../domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  final List<Message> _messages = [
+    Message(text: 'Hola', fromWho: FromWho.me),
+    Message(text: 'Ya volviste del trabajo?', fromWho: FromWho.me),
+  ];
+
+  Future<void> sendMessage(String text) async {
+    // TODO implement method
+  }
+}
